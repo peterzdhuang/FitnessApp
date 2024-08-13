c230084b262925a284230593fbc83ee5e49af20d
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2024-08-13 09:04:58 -0600

    expo initial commit

--- main.py
+++ main.py
@@ -97,7 +97,4 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             break
 
     cap.release()
-    cv2.destroyAllWindows()
-
-
-    
+    cv2.destroyAllWindows()    
